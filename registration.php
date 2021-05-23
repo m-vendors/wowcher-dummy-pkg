8ccdc0816684fa619552083b1db82e667f373ce9
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:41:55 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'wowcher_DummyPkg',
+    __DIR__
+);
